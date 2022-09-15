6dc70bfc1931ae10f961a18328ff98abcf4804fb
Chris Snoek1 <chrissnoek1@MacBook-Pro.local>
2022-09-15 19:27:13 +0200

    add gitignore and move darkmode to top

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: "class",
 	content: [
 		"./src/**/*.{vue,js,ts,jsx,tsx,html}",
 		"./resources/**/*.blade.php",
 		"./resources/**/*.js",
 		"./resources/**/*.vue",
 	],
-	theme: {
-		extend: {},
-	},
 	plugins: [
 		require("@tailwindcss/forms"),
 		require("@tailwindcss/typography"),
